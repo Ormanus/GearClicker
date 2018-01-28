0290cb47bec74511a555828e0c84de8e870771d0
McFish <spamtrap_github@mcfish.org>
2018-01-28 11:40:32 +0200

    fixed gearing

--- Assets/Scripts/GearController.cs
+++ Assets/Scripts/GearController.cs
@@ -6,7 +6,31 @@ public class GearController : MonoBehaviour {
 
     public int gearTeeths;
 
+    private float angleDiff = 0.0f;
     private float angleCounter = 0.0f;
+
+    public void changeCounter(float amount)
+    {
+        angleCounter += amount;
+        angleDiff += amount;
+    }
+
+    public float getCounter()
+    {
+        return angleCounter;
+    }
+
+    public float getCounterDiff()
+    {
+        float retval = angleDiff;
+        angleDiff = 0;
+        return (retval);
+    }
+
+    public void resetCounter()
+    {
+        angleCounter = 0.0f;
+    }
 
     public virtual void changeGear(int newSize)
     {
--- Assets/Scripts/MasterGearController.cs
+++ Assets/Scripts/MasterGearController.cs
@@ -4,8 +4,34 @@ using UnityEngine;
 
 public class MasterGearController : GearController
 {
+    float prevAngle = 0.0f;
+
     public void ApplyImpulse(float force)
     {
         GetComponent<Rigidbody>().AddTorque(new Vector3(0, 0, -force), ForceMode.Impulse);
+    }
+
+    public void Start()
+    {
+        prevAngle = transform.rotation.eulerAngles.z;
+    }
+
+    public void Update()
+    {
+        float offset = transform.rotation.eulerAngles.z - prevAngle;
+        if (offset == 0.0f)
+            return;
+
+        // Did Internal counter reset?
+        if (offset < 0.0f)
+        {
+            offset += 360.0f;
+        }
+
+        //
+        changeCounter(offset);
+
+        //
+        prevAngle = transform.rotation.eulerAngles.z;
     }
 }
